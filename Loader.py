a27049aea89cd88306747ff88f753a505765fb8f
Joel Villarreal II <joel.villa2329@gmail.com>
2026-03-09 11:55:37 -0600

    added helper functions

--- Loader.py
+++ Loader.py
@@ -4,12 +4,13 @@ A class for saving and loading in files
 import numpy as np
 import os
 
+DICT_KEYS = ["ss", "diffs", "num_runs", "mat_size"]
 
 class Loader:
     def __init__(self):
         pass
 
-    def load(self, filename, names):
+    def load(self, filename, names=DICT_KEYS):
         """
         Load in the data from the specified file and return it
         If none exists, return None
@@ -51,8 +52,33 @@ class Loader:
             **data # Note: this is called dictionary unpacking
         )
 
+    def get_keys(self):
+        """
+        Return default data dictionary keys
+        """
+        return DICT_KEYS
+    
+    def default_dict(self, n, ss):
+        """
+        Return a default dictionary, with everything initialized to zero, except
+        num_rows
+
+        n   - number of rows in the matrix
+        ss  - the s values which will be used for testing
+
+        RETURN: 0 dictionary
+        """
+
+        data_dict = {}
+        data_dict['ss'] = ss
+        # print(f"ss.")
+        data_dict['diffs'] = np.zeros(shape=np.shape(ss))
+        data_dict[DICT_KEYS[2]] = 0
+        data_dict[DICT_KEYS[3]] = n
+
+        return data_dict
+
+
 l = Loader()
-dict_one = {"name": "John","age": 25}
-l.save("myFile", dict_one)
-dict = l.load(dict_one.keys)
+dict = l.load("662_bus")
 print(dict)
