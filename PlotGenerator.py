d5bbcd95d3bae0f79c9b97505f29aac7c0ce15e4
Joel Villarreal II <joel.villa2329@gmail.com>
2026-03-09 11:57:36 -0600

    plotting logic moved

--- PlotGenerator.py
+++ PlotGenerator.py
@@ -2,6 +2,7 @@
 A class for loading the plots utilizing Loader class
 """
 from Loader import Loader
+import matplotlib.pyplot as plt
 
 class PlotGenerator:
     def plot(self, name):
@@ -10,4 +11,23 @@ class PlotGenerator:
         """
 
         loader = Loader()
-        
+        data_dict = loader.load(filename=name)
+
+        title = "Sparsification Behavior of " + name + f" (n = {data_dict['mat_size']}, number of runs = {data_dict['num_runs']})"
+
+        x_label = "s"
+        y_label = rf"$||e - \tilde e||$"
+
+        plt.plot(data_dict['ss'], data_dict['diffs'])
+        plt.title(title)
+
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+
+        # plt.savefig("plots/" + name  + ".svg")
+
+        plt.show()
+    
+
+generator = PlotGenerator()
+generator.plot("bcsstk03")
