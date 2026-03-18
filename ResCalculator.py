b56b2b7e30c66f8ea05f2b015f3656d0b827c4f3
Joel Villarreal <jvillarreal3@unm.edu>
2026-03-18 13:44:13 -0600

    Added Residue Calculator object

---
+++ ResCalculator.py
@@ -0,0 +1,53 @@
+"""
+This class is used for checking the validity of a matrix and two vectors that
+supposedly solve Ax = b
+"""
+
+from scipy.linalg import norm
+
+class ResCalculator:
+    def __init__(self, A=None, x=None, b=None):
+        """
+        Initialize the Calculator given some combination of A, x, and b 
+        that is likely unchanging
+        A - a nxm sparse matrix
+        x - a vector of length m
+        b - a vector of length n
+        """
+        self.A = A
+        self.x = x
+        self.b = b
+
+        n, m = A.shape
+        if (m != len(x)):
+            print(f"WARNING: in ResCalculator, {m} = m != len(x) = {len(x)}")
+
+        if (n != len(b)):
+            print(f"WARNING: in ResCalculator, {n} = m != len(b) = {len(b)}")
+
+    def get_res(self, A=None, x=None):
+        """
+        Given a matrix, and two vectors that supposedly solve Ax = b, 
+        calculate the residual and return it
+
+        A - an nxm sparse matrix
+        x - a vector of length m
+
+        RETURN: residual of Ax = b
+        """
+
+        if (A is None):
+            # Set A to initialization A
+            if (self.A is None):
+                print(f"ERROR: A never set in ResCalculator")
+            A = self.A
+
+        if (x is None):
+            # Set x to initialization x
+            if (self.x is None):
+                print(f"ERROR: x never set in ResCalculator")
+            x = self.x
+        
+        res = self.b - A * x
+
+        return (norm(res) / norm(self.b))
