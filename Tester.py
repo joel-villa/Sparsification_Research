3c1ff828e212489585c5f433cc0f31dd7f72b000
Joel Villarreal II <joel.villa2329@gmail.com>
2026-03-22 11:38:35 -0600

    method for comparing the residuals of Ax and ~Ax

--- Tester.py
+++ Tester.py
@@ -4,20 +4,63 @@ A class for testing the behavior of a Sparsifier
 import numpy as np
 from Sparsifier import Sparsifier
 from MatrixChecker import MatrixChecker
+from scipy.linalg import norm
 
 class Tester:
     """
     A class for testing a sparsifier
+
+    sparsifier - the sparsifier which can be used to sparsify a scipy sparse
+                 matrix
+    num_iter   - the number of iterations the tester will do
+    num_ss     - only relevant for test_s_behavior
+    max_s      - only relevant for test_s_behavior
     """
     def __init__(self,
                  sparsifier=Sparsifier(),
-                 num_iter=10, 
+                 num_iter=1, 
                  num_ss=50, 
                  max_s=5):
         self.sparsifier = sparsifier
         self.num_iter   = num_iter
         self.num_ss     = num_ss
         self.max_s      = max_s
+
+    def test(self, A, ss):
+        """
+        Given a scipy sparse matrix A, and some factor of sparsification s, 
+        test how close Ax is to ~Ax, where ~A is the sparsified version of A.
+        Do this for randomly generated x values, num_iter times
+
+        A  - sparse matrix 
+        ss - the s value(s) to sparsify A with
+        """
+
+        residuals = []
+        xs = np.random.rand(A.shape[0], self.num_iter) # randomly generated x vectors
+        
+        for s_val in ss:
+            s_res = []
+            for x in xs:
+                # Sparsify A
+                A_sparse = A.copy()
+                self.sparsifier.sparsify(A_sparse, s_val)
+
+                # Calculate b
+                b = A @ x
+
+                # Calculate sparse b
+                b_sparse = A_sparse @ x
+
+                # Measure and save difference (residual)
+                res = (norm(b - b_sparse) / norm(b))
+                s_res.append(res)
+            residuals.append(s_res)
+
+        # Return residuals
+        return residuals
+        #TODO: test this
+
 
     def test_s_behavior(self, A):
         """
