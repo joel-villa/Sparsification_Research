25c84c8b9dee96fa29ce6a8acbda99a8ba201dbf
Joel Villarreal II <joel.villa2329@gmail.com>
2026-04-15 10:51:48 -0600

    some helper code (from another repo - god my file structure is terrible)

---
+++ src/Plotter.py
@@ -0,0 +1,40 @@
+"""
+For plotting thangs
+"""
+import matplotlib.pyplot as plt
+
+COLORS = ["black", 
+        "#FF6B6B",  
+        "#FFA94DDD", 
+        "#EEF132", 
+        "#6BCB77", 
+        "#54FCD8",
+        "#4D96FF",
+        "#8C77FF",  
+        "#FF7DF0",
+        "#F72B2B"]
+class Plotter():
+    def __init__(self, save_fig, show_fig):
+        self.save_fig = save_fig
+        self.show_fig = show_fig
+        
+    def init_plot(self, title, x_label, y_label, save_name):
+        plt.title(title)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        self.save_path = f"plots/{save_name}.jpg"
+
+    def add_to_plot(self, xs, ys, label):
+        """
+        Add given info to the plot
+        """
+        plt.plot(xs, ys, label=label)
+
+    def finish(self):
+        plt.legend()
+        plt.tight_layout()
+        
+        if self.save_fig:
+            plt.savefig(self.save_path)
+        if self.show_fig:
+            plt.show()
---
+++ src/SGenerator.py
@@ -0,0 +1,99 @@
+"""
+A class for generating apt s-values,
+
+PAPER: https://users.cs.utah.edu/~jeffp/teaching/cs7931-S15/cs7931/8-sparsification.pdf
+
+Goal: what factor of sparsification best converges with the Jacobi itterative 
+method
+"""
+import numpy as np
+
+class SGenerator:
+    def __init__(self, n, nnz, log_base=2):
+        """
+        Some information about the dimensions of the matrix to pick s-values for
+        n        - number of rows and number of columns of the matrix (assuming square)
+        nnz      - number of nonzeros of matrix (assuming sparse)
+        log_base - performing log function in s generation, what base? 
+
+        BEHAVED BEST EMPIRICALLY (so far):
+        Sqrt(nnz) 
+        Log base 2
+
+        """
+        self.n = n
+        self.nnz = nnz
+        #The number of potential sparsified values (not sparsifying diagonal)
+        self.off_diags = self.nnz - self.n
+        self.log = self.get_log_func(log_base)
+
+    def get_log_func(self, log_base):
+        """
+        log_base = 2 -> log2()
+        log_base = anything else -> natural log
+
+        RETURN: a numpy log function
+        """
+        match log_base:
+            # Get log function
+            case 2:
+                return np.log2
+            case _:
+                return np.log
+    
+    def get_min_s(self, x=1):
+        """
+        The minimum s-value s.t. the expected number of nonzeros is 1 or more
+
+        x - the desired expected amount of new nonzeros after sparsification
+
+        This values was found mathematically:
+        E(X) = # of new nonzeros
+        E(X) = n(1-s)
+        E(X) >= x --> s >= n / (n-x)
+            where n is the candidate nonzeros (off diagonal count)
+        """
+        if (self.off_diags - x <= 0):
+            raise ValueError(f"nnz candidates is {self.off_diags}, can't reduce by {x}")
+
+        s = self.off_diags / (self.off_diags - x)
+
+        return s
+
+    def get_safe_s(self, type="nnz"):
+        """
+        Get the maximum possible s-value (i.e. expected number of zeros greater
+        than one, and based on upper bound of sparsification algorithm)
+
+        type - "nnz"        -> square root of the number of zeros in matrix is n
+               "candidates" -> square root of non-diagonals in matrix is n
+               _            -> matrix dimension is n
+        """
+        return max(self.get_min_s(), self.get_s(type))
+
+    def get_s(self, type="nnz"):
+        """
+        Based on upper bound of s in the paper, generate an s
+
+        type - "nnz"        -> square root of the number of zeros in matrix is n
+               "candidates" -> square root of non-diagonals in matrix is n
+               _            -> matrix dimension is n
+        """
+        match type:
+            # Get n value
+            case "nnz":
+                n = np.sqrt(self.nnz)
+            case "candidates":
+                n = np.sqrt(self.off_diags)
+            case _:
+                n = self.n
+        
+        two_n = 2*n
+        
+        #  log2() -> log base 2
+        denominator = 4 * ((self.log(two_n)) ** 6)
+
+        s = (two_n / denominator)
+
+        # Adding one to force to be in valid s-range
+        return s + 1
